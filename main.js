7c25ddc94a90e09c04e9bdd016556f697051ab75
stefanavra <stefan.avra@gmail.com>
2022-04-20 18:37:05 +0200

    added buttons

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("👋\nhttps://github.com/stefanavra");
+console.log("👋\nhttps://avra.dev");
 
 const root = document.querySelector(":root");
 
@@ -11,3 +11,26 @@ document.querySelector("#toggle-color").addEventListener("click", (event) => {
             : "black"
     );
 });
+
+// clock
+// accuracy is ± 60 seconds - just like the real thing
+const timeEl = document.querySelector("time");
+setInterval(syncTimeToClock, 60 * 1000);
+
+syncTimeToClock();
+
+function syncTimeToClock() {
+    const date = new Date();
+    const options = {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: false,
+    };
+    timeEl.innerHTML = new Intl.DateTimeFormat("default", options).format(date);
+    timeEl.dateTime = date;
+}
+
+//power button
+document
+    .querySelector(".power")
+    .addEventListener("click", (event) => console.log(event.target));
